99354cd7d0afff8899d1041f36d7be1b7da66c39
vshal122 <vmalviya803@gmail.com>
2024-12-13 09:44:39 +0530

    add Learning Plateform Repository

---
+++ src/main/java/com/example/learningplateform/LearningplateformApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningplateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningplateformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningplateformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learningplateform/LearningplateformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningplateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningplateformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
